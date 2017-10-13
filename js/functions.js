04b8d4e2f51bfbf8f1bb1ef28eef3850acae73a6
Mary Kathryn Syms <baglady@Marys-MacBook-Pro.local>
2017-10-13 10:04:21 -0500

    Went over Codeup curriculum exercises

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,9 @@
-"use strict";
+(function(){
 
-/**
+   "use strict";
+
+
+    /**
  * TODO:
  * Create a function called 'sayHello' that takes a parameter 'name'.
  * When called, the function should return a message that says hello to the passed in name.
@@ -10,11 +13,10 @@
  */
 
 function sayHello(name) {
-    var name = prompt ("What is your name?");
-    console.log("Hello, " + name + "!");
-}
-sayHello(name);
 
+   return "Hello " + name + "!";
+}
+// console.log(sayHello("Mary"));
 
 
 /**
@@ -25,32 +27,48 @@ sayHello(name);
  * console.log 'helloMessage' to check your work
  */
 
-function helloMessage(name) {
-    console.log(sayHello(name));
-}
-console.log(helloMessage)
+
+// var helloMessage = sayHello("Mary");
+// console.log(helloMessage);
 
 
-
+// sayHello("Mary"); calling the function, we already have it from problem  ^. passing "Mary" as a string literally
+// stored sayHello("Mary") in variable -- var helloMessage = sayHello("Mary");
 
 
 
 /**
  * TODO:
- * Store your name as a string in a variable named 'myName', and pass that
- * variable to the 'sayHello' function. You should see the same output in the
- * console.
+ * Store your name as a string in a variable named 'myName',
+ * and pass that variable to the 'sayHello' function.
+ * You should see the same output in the console.
  */
 
+//not hard coding with var myName because youre OUTSIDE OF THE FUNCTION!!!
+// you dont need to create a new function for sayHello(name) because you previously declared it
+
+var myName = "Mary Kathryn";
+console.log(sayHello(myName));
+
+//call the function and then "passing" the assigned variable (nest variable inside paren)
 
 
+// THIS IS A BIG NO NO!!!!!!
 
+
+// -function sayHello(myName)
+// var myName = "Mary Kathryn";
+// AVOID THIS ---  var myName = "Mary Kathryn" -- INSIDE of a function
+// that is hardcoding information
+// assigning value to a variable, makes function not reusable
+// function is a general formula that you can use to generate outputs with different info you assign your variables
 
 
 
 // Don't modify the following line, it generates a random number between 1 and 3
 // and stores it in a variable named random
 var random = Math.floor((Math.random() * 3) + 1);
+
 
 /**
  * TODO:
@@ -70,15 +88,46 @@ var random = Math.floor((Math.random() * 3) + 1);
  * number)
  */
 
+
+function isTwo(number) {
+   if (number === 2) {
+      return true;
+   } else {
+      return false;
+   }
+
+}
+console.log(isTwo(random));
+
+
+
+
+
 /**
  * TODO:
  * Create a function named 'calculateTip' to calculate a tip on a bill at a
- * restaurant. The function should accept a tip percentage and the total of the
- * bill, and return the amount to tip
+ * restaurant.
+ *
+ * The function should accept a tip percentage
+ * and the total of the bill,
+ *
+ * and return the amount to tip
  *
  * Example:
  * > calculateTip(0.20, 20) // returns 4
  */
+
+
+function calculateTip(tipDecimal, totalBill) {
+   return tipDecimal * totalBill;
+}
+// console.log(calculateTip(.2, 200);
+
+//ORDER THAT YOU PASS IN ARGUMENT MATTERS
+// NEED TO MATCH UP TO PARAMETERS RESPECTIVELY
+
+
+
 
 /**
  * TODO:
@@ -86,3 +135,32 @@ var random = Math.floor((Math.random() * 3) + 1);
  * prompt the user for the bill total and a percentage they would like to tip,
  * then display the dollar amount they should tip
  */
+
+
+var totalBill = prompt ("What is the total bill?");
+var tipPercentage = prompt ("What tip percentage will you use?");
+
+alert("The amount to tip is: " + calculateTip(tipPercentage, totalBill));
+
+
+
+
+//REMEMBER THAT THE ARGUMENTS - NEED TO MATCH UP TO PARAMETERS RESPECTIVELY
+
+
+
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
