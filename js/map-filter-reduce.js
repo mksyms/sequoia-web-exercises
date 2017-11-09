407840249728569344c2d09544c493e016d65183
Mary Kathryn Syms <baglady@Marys-MacBook-Pro.local>
2017-11-08 18:52:12 -0600

    created html and js files for ES6 Map, Filter, Reduce functions excercise

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//-----------------------------------------------------------------------------------//
+//
+// Use .filter
+// to create an array of user objects
+// where each user object has at least 3 languages in the languages array.
+//
+// Use .map
+// to create an array of strings
+// where each element is a user's email address
+//
+// Use .reduce
+// to transform the array into an object
+// where the object's keys are ids and the values are objects that represent each user
+//
